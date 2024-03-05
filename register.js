168c898944c1ad468b390bf893eaa84bbfb472b3
ValdezJamesAndrei <jamesandrei2216@gmail.com>
2024-03-05 16:00:14 +0800

    register.html

---
+++ register.js
@@ -0,0 +1,27 @@
+console.log("Hello Word");
+console.error("ERROR HAPPENED!!");
+console.warn("Warninggg!")
+
+// Variables
+//String="words" , "wowowowo" , '123'
+//number= 123, 434e,2398.12
+//boolean = true , false
+//object = ["apple" , "banana"], [1,2,3,4]
+        //["apple", 3 , true]
+
+//typeof
+//console.log(typeof(["apple","banana"]))  
+
+let name="James",
+lastname ='Valdez',
+    age  = 19;
+    isStudent = true;   
+    hobbies = ["Guitar" , "Watching Movies" , "Playing Video Games","Composing","Producing"], //object
+    run = function(){
+        console.log ("Im running");
+    };
+console.log(name, age, isStudent,hobbies);
+console.table(hobbies);
+console.log("My name is" + name + "" + lastname);
+console.log(`My name is ${name} ${lastname}`);
+
